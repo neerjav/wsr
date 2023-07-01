f5c89bf0d89f8664145684e3f43284dca5e9b8d9
Neeraja V <neerajavudatha@gmail.com>
2023-07-01 21:31:07 +0530

    Update streamlit_wsr.py

--- streamlit_wsr.py
+++ streamlit_wsr.py
@@ -85,8 +85,18 @@ df = pd.DataFrame(
 
 
 
-#displaying the dataframe in a static manner
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
 
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+# Display a static table
 st.table(df)
 
 
