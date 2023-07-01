1c599f8f7da0f4c6f67479237fde882fb5a6a0bd
Neeraja V <neerajavudatha@gmail.com>
2023-07-01 19:14:51 +0530

    Update streamlit_wsr.py

--- streamlit_wsr.py
+++ streamlit_wsr.py
@@ -6,16 +6,30 @@ st.title('WSR Reporting Tool')
 
 #color = st.color_picker('Current Status of the project', '#00f900')
 
-
-option = st.selectbox(
+col1, col2, col3 = st.columns(3)
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+   option = st.selectbox(
     'Client & Project',
     ('MGM', 'Segdewick', 'Juvo'))
 
-st.write('Client selected : ', option)
+   st.write('Client selected : ', option)
 
-st.date_input('SOW Start Date')
+   st.date_input('SOW Start Date')
 
-st.date_input('SOW End Date')
+   st.date_input('SOW End Date')
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
+
+
+
 
 #st.image()
 #st.image()
