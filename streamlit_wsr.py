8eb256a6a3e9be12c7a343c9e6c1a20fa2a34fdd
Neeraja V <neerajavudatha@gmail.com>
2023-07-01 18:11:56 +0530

    testing

--- streamlit_wsr.py
+++ streamlit_wsr.py
@@ -3,7 +3,16 @@ import streamlit as st
 
 st.header('WSR Report')
 
+color = st.color_picker('Pick A Color', '#00f900')
+
+option = st.selectbox(
+    'Please choose a project to view the details',
+    ('MGM', 'Segdewick', 'Juvo'))
 
 
 score = st.slider('Please specify your test score', 0, 100, 10)
 st.write("My test score is ", score)
+
+st.balloons()
+
+st.image('/')
