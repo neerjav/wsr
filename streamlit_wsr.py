9fb622172bec41db0682070246d68ab66469b85a
Neeraja V <neerajavudatha@gmail.com>
2023-07-01 18:13:01 +0530

    Update streamlit_wsr.py

--- streamlit_wsr.py
+++ streamlit_wsr.py
@@ -3,7 +3,8 @@ import streamlit as st
 
 st.header('WSR Report')
 
-color = st.color_picker('Pick A Color', '#00f900')
+color = st.color_picker('Current Status of the project', '#00f900')
+
 
 option = st.selectbox(
     'Please choose a project to view the details',
@@ -13,6 +14,3 @@ option = st.selectbox(
 score = st.slider('Please specify your test score', 0, 100, 10)
 st.write("My test score is ", score)
 
-st.balloons()
-
-st.image('/')
